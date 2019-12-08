d98ba740af793a76826f0496b17565f881d79d65
Jamsies99 <jamsies99@gmail.com>
2019-12-08 18:34:08 -0500

    Add files via upload

---
+++ drive-download-20191125T153944Z-001/human.js
@@ -0,0 +1,29 @@
+function Human(x, y){
+	this.x = x;
+	this.y = y; 
+	this.r = 12.5;
+	this.struck = false; 
+	this.ydir = 3;
+	
+this.show = function(){
+	fill(150,0,0);
+	ellipse(this.x, this.y, this.r*2,this.r*2); 
+	//REPLACE ELLIPSE 
+}
+
+
+this.move = function(){
+	this.y = this.y + this.ydir;
+
+}
+
+this.hit = function(){
+	this.struck = true;
+}
+
+this.shiftLeft = function(){
+	this.x -= this.r; 
+	this.ydir *= -1.1;
+}
+
+}
